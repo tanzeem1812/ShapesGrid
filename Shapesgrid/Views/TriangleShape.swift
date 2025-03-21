ce58e7b75a7795389c9fd3582c9b33b3d9db6781
Tanzeem Ahamad <88631557+tanzeem1812@users.noreply.github.com>
2025-03-21 00:09:36 -0400

    little improvement
    little improvement

--- Shapesgrid/Views/TriangleShape.swift
+++ Shapesgrid/Views/TriangleShape.swift
@@ -16,26 +16,15 @@ struct TriangleShape: ShapeView {
     
     var color: Color = Color.cyan
     var view: AnyView {
-        AnyView(Triangle(topPoint: topPoint,
-                        bottomLeftPoint: bottomLeftPoint,
-                        bottomRightPoint: bottomRightPoint,
-                        color:color)
+        AnyView(
+            Path {
+                $0.move(to: topPoint)
+                $0.addLine(to: bottomLeftPoint)
+                $0.addLine(to: bottomRightPoint)
+                $0.closeSubpath() // close the path is optional, but its a good practice
+            }.fill(color)
         )
     }
 }
 
-struct Triangle: View {
-    let topPoint:CGPoint
-    let bottomLeftPoint:CGPoint
-    let bottomRightPoint:CGPoint
-    let color: Color
 
-    var body: some View {
-        Path {
-            $0.move(to: topPoint)
-            $0.addLine(to: bottomLeftPoint)
-            $0.addLine(to: bottomRightPoint)
-            $0.closeSubpath() // close the path is optional, but its a good practice
-        }.fill(color)
-    }
-}
